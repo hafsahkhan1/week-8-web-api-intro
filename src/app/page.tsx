72a6841972a35114cb680b1ec2e2c17debe2521b
Hafsah <hafsah12khan@gmail.com>
2026-04-06 18:51:26 -0500

    Week 8 Activity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react";
 
+const API_KEY = "insert_key_here";
+
 export default function WeatherApp() {
   const [city, setCity] = useState("San Francisco");
   const [searchInput, setSearchInput] = useState("");
@@ -9,7 +11,19 @@ export default function WeatherApp() {
   const [error, setError] = useState("");
 
   const fetchWeather = async (cityName: string) => {
-    
+    try{
+      setError("");
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`);
+      const data = await response.json();
+
+      if (!response.ok){
+        throw new Error(data.message || "City not found or API error");
+      }
+      setWeather(data);
+    } catch(err: any){
+      setError(err.message);
+      setWeather(null);
+    }
   };
 
   useEffect(() => {
